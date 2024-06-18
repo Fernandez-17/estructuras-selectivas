8e728237aaaf22ca99f17b739539dcaa00c99eed
Fernandez-17 <crfernandezc@unjbg.edu.pe>
2024-06-17 23:47:41 -0500

    Se agrego una condicional que muestra en mensaje de monto de compra invalido para valores negativos

--- Ejercicio_Descuentos.cpp
+++ Ejercicio_Descuentos.cpp
@@ -6,16 +6,22 @@ int main( ){
 	double compra, pago_final;
 	cout<<"Ingrese el monto de su compra realizada: ";
 	cin>>compra;
-	if(compra>1000){
+	if(compra<0){
+		cout<<"El monto de compra ingresado es invalido";
+		}
+		else{
+		if(compra>1000){
 		cout<<"Felicidades el monto de su compra aplica para el descuento."<<endl;
 		pago_final=compra-compra*0.20;
 		cout<<"El monto de su pago final es de: "<<pago_final<<endl;
-	} 
-	else{
+		} 
+		else{
 		cout<<"Lo sentimos mucho, el monto de su compra no aplica para el descuento."<<endl;
 		pago_final=compra;
 		cout<<"El monto de su pago final es de: "<<pago_final<<endl;
+		}
 	}
+
 	return 0;
 } 
- Se agrego un comentario
+ 

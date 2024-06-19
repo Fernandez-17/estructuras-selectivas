ee882c69b39c8f66bbf939897f57bd2ad0e85b69
Fernandez-17 <crfernandezc@unjbg.edu.pe>
2024-06-19 02:15:50 -0500

    Se actualizo el Ejercicio_Descuento de promedios: se añadio una condicional para invalidar montos negativos

--- Ejercicio_Descuento de promedios.cpp
+++ Ejercicio_Descuento de promedios.cpp
@@ -1,9 +1,8 @@
-/*9.	Se ha establecido un programa para estimular a los alumnos, el cual consiste
- en lo siguiente: si el promedio obtenido por un alumno en el último periodo es mayor 
- o igual que 18, se le hará un descuento del 30% sobre la pensión educativa y no se le 
- cobrara IGV; si el promedio obtenido es menor que 18 deberá pagar la pensión educativa 
- completa, y también el 18% de IGV. Obtener cuanto debe pagar un alumno.*/
- 
+/*9.	Se ha establecido un programa para estimular a los alumnos, el cual consiste en lo 
+siguiente: si el promedio obtenido por un alumno en el último periodo es mayor o igual que 18, 
+se le hará un descuento del 30% sobre la pensión educativa y no se le cobrara IGV; si el 
+promedio obtenido es menor que 18 deberá pagar la pensión educativa completa, y también 
+el 18% de IGV. Obtener cuanto debe pagar un alumno.*/
 #include<iostream>
 using namespace std;
 int main( ){
@@ -12,12 +11,17 @@ int main( ){
 	cin>>promedio;
 	cout<<"Ingrese el monto de la pension educativa: ";
 	cin>> pension_educativa;
-	if(promedio>=18){
+	if(pension_educativa<0){
+		cout<<"El monto ingresado no es valido"<<endl;
+	}
+	else{
+		if(promedio>=18){
 		pago_final=pension_educativa-pension_educativa*0.3;
 		cout<<"Usted tiene que pagar un monto de. "<<pago_final<<endl;
-	}else{
+	    }else{
 	    pago_final=pension_educativa+pension_educativa*0.18;
 	    cout<<"Usted tiene que pagar un monto de: "<<pago_final<<endl;
+		}
 	}
 	return 0;
 }
